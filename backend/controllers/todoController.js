3d06f37a8d24db1d90865243ecaac40be8265db4
prakrutivaghasiya <prakrutivaghasiya1@gmail.com>
2022-10-22 18:47:18 -0400

    Connected MongoDB

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Todo = require('../models/todoModel')
+
 // @desc Get Todos
 // @route GET /api/getTodo
 // @access Private
 const getTodo = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get ToDo'})
+    const todos = await Todo.find()
+    
+    res.status(200).json(todos)
 })
 
 // @desc Add Todo
@@ -16,21 +20,45 @@ const addTodo = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Add ToDo'})
+    const todo = await Todo.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(todo)
 })
 
 // @desc Update Todo
 // @route PUT /api/updateTodo
 // @access Private
 const updateTodo = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update ToDo ${req.params.id}`})
+    const todo = await Todo.findById(req.params.id)
+
+    if (!todo){
+        res.status(400)
+        throw new Error('Todo Not Found')
+    }
+
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+
+    res.status(200).json(updatedTodo)
 })
 
 // @desc Delete Todo
 // @route DEL /api/deleteTodo
 // @access Private
 const deleteTodo = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete ToDo ${req.params.id}`})
+    const todo = await Todo.findById(req.params.id)
+
+    if (!todo){
+        res.status(400)
+        throw new Error('Todo Not found')
+    }
+
+    await todo.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
